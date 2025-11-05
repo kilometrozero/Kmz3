b4c9ba3d195041e7c4f2d43a0f621a5dcf5a2d64
kilometrozero <kilometrozerokz@gmail.com>
2025-11-05 09:18:36 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 export default function Home() {
   return (
-    <div style={{ padding: "80px", textAlign: "center" }}>
-      <h1 style={{ fontSize: "3em", color: "#ff0055" }}>KMZ está online 🚀</h1>
-      <p style={{ fontSize: "1.2em", color: "#aaa" }}>Tu tienda urbana está lista.</p>
+    <div style={{ padding: "50px", textAlign: "center" }}>
+      <h1>KMZ funcionando 🚀</h1>
     </div>
   );
 }
